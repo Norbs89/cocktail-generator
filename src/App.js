a04a040cd3ff3341977e173aa84f1956ad5a27ae
AVSheard <a_sheard@yahoo.co.uk>
2020-03-05 16:59:01 +0000

    got the bare bones functionality working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+import recipes from "./recipes.js";
+
+const IngredientChoice = function(props) {
+	return (
+		<div>
+			<h1>Cocktail Generator</h1>
+			<form>
+				<h3>Spirits</h3>
+				<label>
+					<input
+						type="radio"
+						name="spirit"
+						value="vodka"
+						onChange={props.handleChange}></input>
+					vodka
+				</label>
+				<label>
+					<input
+						type="radio"
+						name="spirit"
+						value="rum"
+						onChange={props.handleChange}></input>
+					rum
+				</label>
+				<label>
+					<input
+						type="radio"
+						name="spirit"
+						value="tequila"
+						onChange={props.handleChange}></input>
+					tequila
+				</label>
+			</form>
+		</div>
+	);
+};
+
+class App extends React.Component {
+	state = {
+		ingredients: [
+			{
+				spirit: "vodka",
+				mixer: "tomato juice",
+				extra: "tabasco",
+				drink: "bloodyMary"
+			},
+			{
+				spirit: "rum",
+				mixer: "pineapple juice",
+				extra: "piece of fruit",
+				drink: "pinaColada"
+			}
+		],
+		userInput: "",
+		outputRecipe: ""
+	};
+	handleChange = (event) => {
+		this.setState({ userInput: event.target.value });
+	};
+	generateRecipe = () => {
+		const recipe = "";
+		this.state.ingredients.forEach((ingredient) => {
+			if (this.state.userInput === ingredient.spirit) {
+				this.setState({ outputRecipe: recipes[ingredient.drink] });
+			}
+		});
+	};
+	render() {
+		console.log(this.state.outputRecipe);
+		return (
+			<div>
+				<IngredientChoice
+					ingredients={this.state.ingredients}
+					userInput={this.state.userInput}
+					handleChange={this.handleChange}
+				/>
+
+				<button onClick={this.generateRecipe}>Generate Recipe</button>
+				<p>{this.state.outputRecipe}</p>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/recipes.js
@@ -0,0 +1,10 @@
+/** @format */
+
+const recipes = {
+	bloodyMary:
+		"Method Place the ice in a large jug. Measure the vodka, tomato juice and lemon juice and pour it straight onto the ice. Add 3 shakes of Worcestershire sauce and Tabasco (or more if you like it very spicy) and a pinch of celery salt and pepper. Stir until the outside of the jug feels cold, then strain the cocktail into 2 tall glasses. Top up with fresh ice, add a celery stick and lemon slice to both glasses and enjoy.",
+	pinaColada:
+		"Pulse all the ingredients along with a handful of ice in a blender until smooth. Pour into a tall glass and garnish as you like."
+};
+
+export default recipes;
