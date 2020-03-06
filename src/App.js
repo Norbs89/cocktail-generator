7d73d29e33b43080ac217d42ab2d1f3285498bda
Norbs89 <n_huvos@live.co.uk>
2020-03-06 08:38:12 +0000

    added recipes into our refDB, popup implemented

--- src/App.js
+++ src/App.js
@@ -3,87 +3,145 @@
 import React from "react";
 import "./App.css";
 import recipes from "./recipes.js";
+import Modal from "react-modal";
 
 const IngredientChoice = function(props) {
-	return (
-		<div>
-			<h1>Cocktail Generator</h1>
-			<form>
-				<h3>Spirits</h3>
-				<label>
-					<input
-						type="radio"
-						name="spirit"
-						value="vodka"
-						onChange={props.handleChange}></input>
-					vodka
-				</label>
-				<label>
-					<input
-						type="radio"
-						name="spirit"
-						value="rum"
-						onChange={props.handleChange}></input>
-					rum
-				</label>
-				<label>
-					<input
-						type="radio"
-						name="spirit"
-						value="tequila"
-						onChange={props.handleChange}></input>
-					tequila
-				</label>
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Cocktail Generator</h1>
+      <form>
+        <h3>Spirits</h3>
+        <label>
+          <input
+            type="radio"
+            name="spirit"
+            value="vodka"
+            onChange={props.handleChange}
+          ></input>
+          vodka
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="spirit"
+            value="rum"
+            onChange={props.handleChange}
+          ></input>
+          rum
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="spirit"
+            value="tequila"
+            onChange={props.handleChange}
+          ></input>
+          tequila
+        </label>
+      </form>
+    </div>
+  );
 };
 
 class App extends React.Component {
-	state = {
-		ingredients: [
-			{
-				spirit: "vodka",
-				mixer: "tomato juice",
-				extra: "tabasco",
-				drink: "bloodyMary"
-			},
-			{
-				spirit: "rum",
-				mixer: "pineapple juice",
-				extra: "piece of fruit",
-				drink: "pinaColada"
-			}
-		],
-		userInput: "",
-		outputRecipe: ""
-	};
-	handleChange = (event) => {
-		this.setState({ userInput: event.target.value });
-	};
-	generateRecipe = () => {
-		const recipe = "";
-		this.state.ingredients.forEach((ingredient) => {
-			if (this.state.userInput === ingredient.spirit) {
-				this.setState({ outputRecipe: recipes[ingredient.drink] });
-			}
-		});
-	};
-	render() {
-		console.log(this.state.outputRecipe);
-		return (
-			<div>
-				<IngredientChoice
-					ingredients={this.state.ingredients}
-					userInput={this.state.userInput}
-					handleChange={this.handleChange}
-				/>
+  state = {
+    ingredients: [
+      {
+        spirit: "vodka",
+        mixer: "tomato juice",
+        extra: "tabasco",
+        drink: "bloodyMary"
+      },
+      {
+        spirit: "rum",
+        mixer: "pineapple juice",
+        extra: "piece of fruit",
+        drink: "pinaColada"
+      }
+    ],
+    userInput: "",
+    outputRecipe: "",
+    isOpen: false,
+    drinks: [
+      {
+        name: "Appletini",
+        spirit: "vodka",
+        mixer: "cloudy apple juice",
+        extra: "lemon juice",
+        recipe: "appletini"
+      },
+      {
+        name: "PiNa Colada",
+        spirit: "rum",
+        mixer: "pineapple juice",
+        extra: "piece of fruit",
+        recipe: "pinaColada"
+      },
+      {
+        name: "Tequila Sunrise",
+        spirit: "tequila",
+        mixer: "orange juice",
+        extra: "grenadine",
+        recipe: "tequilaSunrise"
+      },
+      {
+        name: "Screwdriver",
+        spirit: "vodka",
+        mixer: "orange juice",
+        extra: "bitter liqueur",
+        recipe: "screwdriver"
+      },
+      {
+        name: "Rum Babalu",
+        spirit: "rum",
+        mixer: "ginger beer",
+        extra: "bitter liqueur",
+        recipe: "rumBabalu"
+      }
+    ]
+  };
+  handleChange = event => {
+    this.setState({ userInput: event.target.value });
+  };
 
-				<button onClick={this.generateRecipe}>Generate Recipe</button>
-				<p>{this.state.outputRecipe}</p>
-			</div>
-		);
-	}
+  closePopup = event => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
+  generateRecipe = () => {
+    const recipe = "";
+    this.state.ingredients.forEach(ingredient => {
+      if (this.state.userInput === ingredient.spirit) {
+        this.setState({
+          outputRecipe: recipes[ingredient.drink],
+          isOpen: !this.state.isOpen
+        });
+      }
+    });
+  };
+
+  render() {
+    console.log(this.state.outputRecipe);
+    return (
+      <div>
+        <IngredientChoice
+          ingredients={this.state.ingredients}
+          userInput={this.state.userInput}
+          handleChange={this.handleChange}
+        />
+
+        <button onClick={this.generateRecipe}>Generate Recipe</button>
+
+        <Modal className="popup" isOpen={this.state.isOpen}>
+          <div className="popup__text">
+            <h2>Drink name</h2>
+            <p>{this.state.outputRecipe}</p>
+          </div>
+          <button onClick={this.closePopup}>Close</button>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/recipes.js
+++ src/recipes.js
@@ -1,10 +1,18 @@
 /** @format */
 
 const recipes = {
-	bloodyMary:
-		"Method Place the ice in a large jug. Measure the vodka, tomato juice and lemon juice and pour it straight onto the ice. Add 3 shakes of Worcestershire sauce and Tabasco (or more if you like it very spicy) and a pinch of celery salt and pepper. Stir until the outside of the jug feels cold, then strain the cocktail into 2 tall glasses. Top up with fresh ice, add a celery stick and lemon slice to both glasses and enjoy.",
-	pinaColada:
-		"Pulse all the ingredients along with a handful of ice in a blender until smooth. Pour into a tall glass and garnish as you like."
+  bloodyMary:
+    "1: Measure the vodka, tomato juice and a touch of lemon juice and pour it straight onto the ice. 2: Add 3 shakes of Tabasco (or more if you like it very spicy) and stir until the outside of the jug feels cold, then strain the cocktail into 2 tall glasses. 3: Top up with fresh ice, add a celery stick and lemon slice to both glasses and enjoy.",
+  pinaColada:
+    "Pulse all the ingredients along with a handful of ice in a blender until smooth. Pour into a tall glass and garnish as you like.",
+  appletini:
+    "Shake 50ml vodka, 50ml cloudy apple juice, 1 tsp lemon juice and 1 tsp maple syrup (optional) in an ice-filled shaker until the outside of the shaker feels very cold. Strain into a martini glass and garnish with a slice of apple.",
+  screwdriver:
+    "Squeeze the juice from 1 large or 2 small oranges and 1 clementine and set aside. Put a handful of ice cubes into a tall glass and pour over 50ml vodka followed by the fruit juices. Stir gently to combine then add a few dashes of  bitter liqueur and a wedge of orange to garnish.",
+  tequilaSunrise:
+    "In a highball glass filled with ice cubes, pour the tequila and orange juice. Stir well, then Slowly pour the grenadine around the inside edge of the glass. It will sink and gradually rise to mix with the other ingredients.",
+  rumBabalu:
+    "Add all ingredients in a shaker except ginger beer, add ice and shake well until chilled. Strain over fresh ice in a double rocks glass, top with ginger beer and garnish with an orange peel."
 };
 
 export default recipes;
